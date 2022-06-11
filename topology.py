4a818aa9586582568e9c592e55cc4f6b3ff1fed3
Yasir Khairul Malik <yasirkm3071@gmail.com>
2022-06-11 17:44:14 +0700

    deleted refactored lines

--- topology.py
+++ topology.py
@@ -1,15 +1,11 @@
 '''
     Topology untuk tugas besar mata kuliah jaringan komputer
-    -Yasir Khairul Malik (1301204395)
+    - Yasir Khairul Malik (1301204395)
 '''
 
-
-from mininet.net import Mininet
 from mininet.topo import Topo
 from mininet.node import CPULimitedHost
 from mininet.link import TCLink
-from mininet.cli import CLI
-from mininet.log import setLogLevel
 
 class Tubes(Topo):  # CLO1
     '''
@@ -164,28 +160,3 @@ class Tubes(Topo):  # CLO1
 
         for (node1, node2), param in zip(link_pairs,link_params):
             self.addLink(node1, node2, **param)
-
-def ping_local_subnet(net): # CLO1
-    '''
-        Ping all subnet in tubes' topology locally
-    '''
-    A, B, R1, R2, R3, R4 = net.get('A', 'B', 'R1', 'R2', 'R3', 'R4')
-    A.cmdPrint('ping -c 4 192.168.0.1')     # A to R!
-    A.cmdPrint('ping -c 4 192.168.1.1')     # A to R2
-    B.cmdPrint('ping -c 4 192.168.2.1')     # B to R3
-    B.cmdPrint('ping -c 4 192.168.3.1')     # B to R4
-    R1.cmdPrint('ping -c 4 192.168.255.2')  # R1 to R3
-    R1.cmdPrint('ping -c 4 192.168.255.6')  # R1 to R4
-    R2.cmdPrint('ping -c 4 192.168.255.10') # R2 to R3
-    R2.cmdPrint('ping -c 4 192.168.255.14') # R2 to R4
-
-def main():
-    setLogLevel('info')
-    net = Mininet(Tubes())
-    net.start()
-    ping_local_subnet(net)
-    CLI(net)
-    net.stop()
-
-if __name__ == '__main__':
-    main()
